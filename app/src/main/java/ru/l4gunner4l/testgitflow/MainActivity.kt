3566d6c5334806deeb71618b0852b9c2bafe592d
l4gunner4l <mix.nik155@gmail.com>
2020-09-09 20:17:51 +0300

    created dev

--- app/src/main/java/ru/l4gunner4l/testgitflow/MainActivity.kt
+++ app/src/main/java/ru/l4gunner4l/testgitflow/MainActivity.kt
@@ -1,11 +1,12 @@
 package ru.l4gunner4l.testgitflow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // feature
     }
 }
