b7ae28eee15b1b84f5e0298d40d0502715a4d025
l4gunner4l <mix.nik155@gmail.com>
2020-09-09 20:13:55 +0300

    Initial commit

---
+++ app/src/main/java/ru/l4gunner4l/testgitflow/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.l4gunner4l.testgitflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
